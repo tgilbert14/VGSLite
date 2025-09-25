e420097bf3e9bc28b5342f0089b21f70d0c89b6c
sarahwomack <ssalwin@icloud.com>
2025-09-25 16:56:57 -0700

    UI update for cleaning database

--- scripts/cleanOrphanLinks.R
+++ scripts/cleanOrphanLinks.R
@@ -1,43 +1,84 @@
 # Clean out orphan data
 
 result <- dbExecute(mydb, clear.orphan.siteClass)
-if (result > 0) {
-  shinyjs::alert("✅ Cleaned SiteClassLink orphans!")
-} else {
-  shinyjs::alert("⚠️ No orphan'd SiteClassLinks.")
-}
-Sys.sleep(0.5)
+
+showModal(modalDialog(
+  title = "Cleaning database...",
+  tags$div(
+    style = "color: seagreen; font-weight: bold; margin-bottom: 10px;",
+    paste0(result, " orphaned Site Class Links cleaned ✅")
+  ),
+  footer = tagList(
+    modalButton("OK"),
+  ),
+  #easyClose = TRUE
+))
+Sys.sleep(1.5)
 
 result <- dbExecute(mydb, clear.orphan.protocol)
-if (result > 0) {
-  shinyjs::alert("✅ Cleaned Protocol orphans!")
-} else {
-  shinyjs::alert("⚠️ No orphan'd Protocols.")
-}
-Sys.sleep(0.5)
+showModal(modalDialog(
+  title = "Cleaning database...",
+  tags$div(
+    style = "color: seagreen; font-weight: bold; margin-bottom: 10px;",
+    paste0(result, " orphaned Protocols cleaned ✅")
+  ),
+  footer = tagList(
+    modalButton("OK"),
+  ),
+  easyClose = TRUE
+))
+Sys.sleep(1.5)
 
 result <- dbExecute(mydb, clear.orphan.typeList)
-if (result > 0) {
-  shinyjs::alert("✅ Cleaned unused Protocols in TypeList!")
-} else {
-  shinyjs::alert("⚠️ All Protocols in TypeList being used.")
-}
-Sys.sleep(0.5)
+showModal(modalDialog(
+  title = "Cleaning database...",
+  tags$div(
+    style = "color: seagreen; font-weight: bold; margin-bottom: 10px;",
+    paste0(result, " unsued Protocols removed from Protocol Manager ✅")
+  ),
+  footer = tagList(
+    modalButton("OK"),
+  ),
+  easyClose = TRUE
+))
+Sys.sleep(1.5)
 
 result <- dbExecute(mydb, clear.orphan.contactLink)
-if (result > 0) {
-  shinyjs::alert("✅ Cleaned ContactLink orphans!")
-} else {
-  shinyjs::alert("⚠️ No orphan'd ContactLinks.")
-}
-Sys.sleep(0.5)
+showModal(modalDialog(
+  title = "Cleaning database...",
+  tags$div(
+    style = "color: seagreen; font-weight: bold; margin-bottom: 10px;",
+    paste0(result, " orphaned Contact Links cleaned ✅")
+  ),
+  footer = tagList(
+    modalButton("OK"),
+  ),
+  easyClose = TRUE
+))
+Sys.sleep(1.5)
 
 result <- dbExecute(mydb, clear.orphan.contact)
-if (result > 0) {
-  shinyjs::alert("✅ Cleaned unused Contacts!")
-} else {
-  shinyjs::alert("⚠️ All Contacts being used.")
-}
-Sys.sleep(0.5)
+showModal(modalDialog(
+  title = "Cleaning database...",
+  tags$div(
+    style = "color: seagreen; font-weight: bold; margin-bottom: 10px;",
+    paste0(result, " unused Contact(s) removed from Contact Manager ✅")
+  ),
+  footer = tagList(
+    modalButton("OK"),
+  ),
+  easyClose = TRUE
+))
+Sys.sleep(1.5)
 
-shinyjs::alert("✨ Complete! ☑") 
+showModal(modalDialog(
+  title = "Cleaned database ✨",
+  tags$div(
+    style = "color: green; font-weight: bold; margin-bottom: 10px;",
+    paste0("Cleaning database process complete!")
+  ),
+  footer = tagList(
+    modalButton("OK"),
+  ),
+  easyClose = TRUE
+))
